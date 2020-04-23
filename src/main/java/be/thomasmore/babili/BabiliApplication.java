1322d62d38c4f3265a2d312c4b3e032d7bc4d908
Lies Van der Haegen <vdhlies@gmail.com>
2020-04-23 15:53:29 +0200

    Initial commit

---
+++ src/main/java/be/thomasmore/babili/BabiliApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.babili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabiliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabiliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/babili/BabiliApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.babili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabiliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
