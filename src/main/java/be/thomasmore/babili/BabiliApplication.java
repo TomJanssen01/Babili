ffd1deaf0cccf554f1ba7521371e8f78b02584c5
Lies Van der Haegen <vdhlies@gmail.com>
2020-04-23 16:09:32 +0200

    Restored main

---
+++ src/main/java/be/thomasmore/babili/BabiliApplication.java
@@ -0,0 +1,11 @@
+package be.thomasmore.babili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabiliApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BabiliApplication.class, args);
+    }
+}
