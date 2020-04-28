635cdf8cb234fdd0a1cceadb265284d6c8c8b797
r0785168@student.thomasmore.be <Md8118776>
2020-04-28 14:15:30 +0200

    Start and Stop function added.

--- src/main/java/be/thomasmore/babili/HomeController.java
+++ src/main/java/be/thomasmore/babili/HomeController.java
@@ -5,27 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model){
+    public String home(Model model) {
         model.addAttribute("rec", "Klik op start om de taak op te nemen.");
         return "home";
     }
 
-    @PostMapping("/")
-    public String homeRec(Model model){
-        model.addAttribute("rec", "Recording");
+    @GetMapping("/start")
+    public String startRec(Model model) {
+        model.addAttribute("rec", "Rec");
         JavaSoundRecorder.startRec();
-        return "home";
+        return "start";
     }
 
-//    @PostMapping("/stop")
-//    public String homeStop(Model model){
-//        model.addAttribute("rec", "Saving Recording");
-//        JavaSoundRecorder.stopRec();
-//        return "redirect:/";
-//    }
+    @GetMapping("/stopRec")
+    public String stopRec(Model model) {
+        model.addAttribute("rec", "Rec stopped.");
+        JavaSoundRecorder.stopRec();
+        return "stopRec";
+    }
 }
