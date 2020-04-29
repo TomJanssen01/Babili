56616dfdcd39fcd53d010178c3caa9ca4b8a78fa
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-04-29 17:03:07 +0200

    7.1 Authorization gedaan

--- src/main/java/be/thomasmore/babili/HomeController.java
+++ src/main/java/be/thomasmore/babili/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
     @EnableWebSecurity
     public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers("/admin/**").authenticated()
+            http.authorizeRequests().antMatchers("/admin/**").hasAnyAuthority("ADMIN")
                     .anyRequest().permitAll().and().formLogin();
             http.csrf().ignoringAntMatchers("/h2-console/**").and()
                     .headers().frameOptions().sameOrigin();
@@ -61,16 +61,20 @@ public class HomeController {
     }
 
     @GetMapping("/test1")
-    public String test1(Principal principal){
+    public String test1(Principal principal, Model model){
         String loggedInName = principal != null ? principal.getName() : "nobody";
         logger.info("logged in: " + loggedInName);
+        model.addAttribute("name", naam);
+        model.addAttribute("login", loggedInName);
         return "test1";
     }
 
     @GetMapping("/admin/test2")
-    public String test2(Principal principal){
+    public String test2(Principal principal, Model model){
         String loggedInName = principal != null ? principal.getName() : "nobody";
         logger.info("logged in: " + loggedInName);
+        model.addAttribute("name", naam);
+        model.addAttribute("login", loggedInName);
         return "test2";
     }
 }
