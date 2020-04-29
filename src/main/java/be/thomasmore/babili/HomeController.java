d6e79cbd3818ab6676ed98e41f429c5b3805c061
Lies Van der Haegen <vdhlies@gmail.com>
2020-04-29 12:06:17 +0200

    Added test mappings in HomeController

--- src/main/java/be/thomasmore/babili/HomeController.java
+++ src/main/java/be/thomasmore/babili/HomeController.java
@@ -10,4 +10,34 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
+    @GetMapping("/login-student")
+    public String loginStudent(){
+        return "login-student";
+    }
+
+    @GetMapping("/login-teacher")
+    public String loginTeacher(){
+        return "login-teacher";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/register-student")
+    public String registerStudent(){
+        return "register-student";
+    }
+
+    @GetMapping("/register-teacher")
+    public String registerTeacher(){
+        return "register-teacher";
+    }
+
+    @GetMapping("/overview-tasks")
+    public String overviewTasks(){
+        return "overview-tasks";
+    }
 }
