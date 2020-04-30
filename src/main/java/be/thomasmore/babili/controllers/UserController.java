63c16d70de3cb48369b4fc1cc868789702ccd8fd
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-04-30 14:29:10 +0200

    register page gemaakt

--- src/main/java/be/thomasmore/babili/controllers/UserController.java
+++ src/main/java/be/thomasmore/babili/controllers/UserController.java
@@ -1,9 +1,21 @@
 package be.thomasmore.babili.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/register")
+    public String register(Principal principal, Model model){
+        if (principal != null){
+            return "redirect:/";
+        }
+        return "/user/register";
+    }
 }
