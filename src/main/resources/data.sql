c8bf752fc64b13c2707a6ba4c24ab7fc5d25e2bd
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-05-01 14:25:09 +0200

    testdata veranderd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,8 @@
 INSERT INTO USER
 (ID, USERNAME, PASSWORD, ROLE)
 VALUES
-(nextval('USER_SEQ'), 'admin', '$2a$10$fNEP0vLPDboAhB7ZM1vUxOzsj2Wt5IsY1hH3FYLUaE4YyQYFAZVOC', 'ADMIN');
+(nextval('USER_SEQ'), 'admin@admin.admin', '$2a$10$fNEP0vLPDboAhB7ZM1vUxOzsj2Wt5IsY1hH3FYLUaE4YyQYFAZVOC', 'ADMIN');
 INSERT INTO USER
 (ID, USERNAME, PASSWORD, ROLE)
 VALUES
-(nextval('USER_SEQ'), 'talha@talha.talha', '$2a$10$jwHSeW.gbG5SUEuN9XmnweGobyQFj.vZU0xwVf0jrrE1t7jYwy2Hy', 'TEACHER');
+(nextval('USER_SEQ'), 'talha@talha.talha', '$2a$10$jwHSeW.gbG5SUEuN9XmnweGobyQFj.vZU0xwVf0jrrE1t7jYwy2Hy', 'STUDENT');
