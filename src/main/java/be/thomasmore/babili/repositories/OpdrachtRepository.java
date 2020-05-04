7420e2ccba68ae556d74008bd2868f05a4a681df
Tom Janssen <tom.janssen@student.thomasmore.be>
2020-05-04 14:53:08 +0200

    data opdracht toegevoegd in data.sql (samen met Stefaan)

---
+++ src/main/java/be/thomasmore/babili/repositories/OpdrachtRepository.java
@@ -0,0 +1,8 @@
+package be.thomasmore.babili.repositories;
+
+import be.thomasmore.babili.model.Opdracht;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OpdrachtRepository extends CrudRepository<Opdracht, Integer> {
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,8 @@ insert into Cursus
 (ID, NAAM, BESCHRIJVING, docent_Id)
 values
 (0, 'testcursus1', 'Om te testen', 2);
+
+insert into opdracht
+(ID, TITEL, OPGAVE, VOORBEELD, BEOORDELING, CURSUS_Id)
+values
+(0, 'bakker', 'ga naar een test', 'bestel een brood', 'moeilijk', 0);
