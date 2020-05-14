816903e71938075ac6d5ab8a8fe64282a882a5ab
r0785168@student.thomasmore.be <Md8118776>
2020-05-14 14:50:06 +0200

    Cache in commentaar gezet. Aanpassing HTML voor logo.

--- src/main/java/be/thomasmore/babili/controllers/SoundController.java
+++ src/main/java/be/thomasmore/babili/controllers/SoundController.java
@@ -1,10 +1,12 @@
 package be.thomasmore.babili.controllers;
 
+
 import be.thomasmore.babili.audio.JavaSoundPlayer;
 import be.thomasmore.babili.audio.JavaSoundRecorder;
 import be.thomasmore.babili.model.Opdracht;
 import be.thomasmore.babili.repositories.OpdrachtRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,36 +42,17 @@ public class SoundController {
     @GetMapping("/stop/{id}")
     public String stopRec(@PathVariable(required = false) int id, Model model) {
         JavaSoundRecorder.stopRec();
+//        evictAllCaches();
         return "redirect:/user/task-details/" + id + "/opname";
     }
 
-    @GetMapping("/startExample/{id}")
-    public String startExample(@PathVariable(required = false) int id,
-                               Model model) {
-        Optional<Opdracht> optionalOpdracht = opdrachtRepository.findById(id);
-        Opdracht opdrachtFromDB = null;
-        if (optionalOpdracht.isPresent()) {
-            opdrachtFromDB = optionalOpdracht.get();
-        }
-        JavaSoundPlayer.play(opdrachtFromDB.getVoorbeeld());
-        return "redirect:/user/task-details/" + id;
-    }
-
-//    @GetMapping("/listenRec/{id}/opname")
-//    public String listenRec(@PathVariable(required = false) int id,
-//                            Model model,
-//                            Principal principal){
-//        String user = null;
-//        Optional<Opdracht> optionalOpdracht = opdrachtRepository.findById(id);
-//        Opdracht opdrachtFromDB = null;
-//        if (optionalOpdracht.isPresent()) {
-//            opdrachtFromDB = optionalOpdracht.get();
-//        }
-//        if (principal != null) {
-//            user = principal.getName();
-//        }
-//        String pathName = "src/main/resources/static/audioFiles/" + opdrachtFromDB.getTitel() + "/" + user + ".wav";
-//        JavaSoundPlayer.play(pathName);
-//        return "redirect:/user/task-details/" + id + "/opname";
+//    @Autowired
+//    CacheManager cacheManager;
+//
+//    public void evictAllCaches() {
+//        cacheManager.getCacheNames().stream()
+//                .forEach(cacheName -> cacheManager.getCache(cacheName).clear());
 //    }
+
+
 }
