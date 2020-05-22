e232abe50c809b8301a6ae51f87a01c6aa6d50e8
r0785168@student.thomasmore.be <Md8118776>
2020-05-22 20:22:34 +0200

    Visualiser aangemaakt. Microphone stop aangemaakt.

--- src/main/resources/static/js/audio.js
+++ src/main/resources/static/js/audio.js
@@ -1,16 +1,22 @@
 const recordButton = document.getElementById('rec');
 const inleveringButton = document.getElementById('inlevering');
 const stopButton = document.getElementById('stop');
+const playButton = document.getElementById('play');
 let mic, recorder, soundFile;
+var fft;
+var w;
 
 
-
-function setup(){
-    cnv = createCanvas(200,200);
+function setup() {
+    cnv = createCanvas(200, 200);
     cnv.position(recordButton.offsetLeft, recordButton.offsetTop);
     cnv.hide();
     stopButton.style.display = 'none';
+    playButton.style.display = 'none';
+    colorMode(HSB);
     mic = new p5.AudioIn();
+    fft = new p5.FFT(0.7, 64);
+    fft.setInput(mic);
 
     // prompts user to enable their browser mic
     mic.start();
@@ -24,25 +30,39 @@ function setup(){
     // this sound file will be used to
     // playback & save the recording
     soundFile = new p5.SoundFile();
+    w = 350 / 64;
 }
 
-function draw(){
-    background(0.1);
+function draw() {
+    background('rgba(241, 202, 0, 1)');
+    var spectrum = fft.analyze();
+    console.log(spectrum);
+    noStroke();
+    for (var i = 0; i < spectrum.length; i++) {
+        var amp = spectrum[i];
+        var y = map(amp, 0, 256, height, 0);
+        fill(i, 255, 255);
+        rect(i * w, y, w-2, height - y);
+    }
 }
 
-recordButton.addEventListener('click', () =>{
+recordButton.addEventListener('click', () => {
     recordButton.style.display = 'none';
     cnv.show();
     stopButton.style.display = 'block';
     stopButton.style.marginLeft = 210 + 'px';
     inleveringButton.style.marginTop = 170 + 'px';
     userStartAudio();
-    if (mic.enabled){
+    if (mic.enabled) {
         recorder.record(soundFile);
     }
 });
 
-stopButton.addEventListener('click', ()=>{
+stopButton.addEventListener('click', () => {
     recorder.stop();
+    mic.stop();
     stopButton.style.display = 'none';
+    playButton.style.display = 'block';
+    playButton.style.marginLeft = 210 + 'px';
+
 })
